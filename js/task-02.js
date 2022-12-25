091c738f4fa69716bc40f7f0b2a6c93756a0e6b2
AntonRoston <rostonanton@gmail.com>
2022-12-25 11:22:55 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,34 @@
+const ul = document.getElementById("ingredients");
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+//* for...of
+// for (const ingredient of ingredients){
+//   const li = document.createElement("li")
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   ul.append(li)
+// }
+
+//* map
+// const liEl = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join(" ");
+// console.log("🚀 ~ liEl", liEl);
+
+// ul.insertAdjacentHTML("beforeend", liEl);
+// // ul.innerHTML = liEl;
+
+//* reduce
+
+const liCon = ingredients.reduce(
+  (acc, ingredient) => (acc += `<li class="item">${ingredient}</li>`),
+  ""
+);
+console.log("🚀 ~ liCon", liCon);
+ul.insertAdjacentHTML("beforeend", liCon);
